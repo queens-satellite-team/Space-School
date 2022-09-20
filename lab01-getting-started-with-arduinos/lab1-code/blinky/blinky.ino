39b63a9b7c56de84661719434d05e4bf6a26897a
SydneyCaulfeild <47483016+SydneyCaulfeild@users.noreply.github.com>
2022-09-20 14:46:57 -0400

    Update blinky.ino

--- lab01-getting-started-with-arduinos/lab1-code/blinky/blinky.ino
+++ lab01-getting-started-with-arduinos/lab1-code/blinky/blinky.ino
@@ -1,15 +1,15 @@
 // Title:         Blinky Program
-// Last Updated:  September 15th, 2021
+// Last Updated:  September 20th, 2022
 // Author:        < your name here >
 // Breif:         Simple program to make sure our hardware setup is working.  
 
 void setup() {
-  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(8, HIGH);
+  digitalWrite(12, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(8, LOW);
+  digitalWrite(12, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
